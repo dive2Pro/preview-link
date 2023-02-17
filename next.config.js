134758f183248fcefa828814e354e6f39d9c6f91
dive2Pro <toylucklebo@gmail.com>
2023-02-18 01:37:26 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
           { key: "Access-Control-Allow-Credentials", value: "true" },
           {
             key: "Access-Control-Allow-Origin",
-            value: "https://roamresearch.com/",
+            value: "https://roamresearch.com",
           },
           {
             key: "Access-Control-Allow-Methods",
